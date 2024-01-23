78ce0ee3310d27854ef0523cfdd0bd0a73526786
LiuBo <g.user.lb@gmail.com>
2024-01-23 13:51:25 +0800

    delete cases

--- test/distributed/cases/tenant/privilege/create_account_case_insensitive.sql
+++
@@ -1,9 +0,0 @@
-set global enable_privilege_cache = off;
-drop account if exists NtrWE02sgtvCXu;
-create account NtrWE02sgtvCXu admin_name '謳95071驊2连5202曹' identified by '鑊78狿殻01948楲777';
-create account NtrWE02sgtvCXu admin_name '謳95071驊2连5202曹' identified by '鑊78狿殻01948楲777';
-create account if not exists NtrWE02sgtvCXu admin_name '謳95071驊2连5202曹' identified by '鑊78狿殻01948楲777';
-drop account NtrWE02sgtvCXu;
-drop account NtrWE02sgtvCXu;
-drop account if exists NtrWE02sgtvCXu;
-set global enable_privilege_cache = on;
